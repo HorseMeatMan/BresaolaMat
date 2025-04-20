366c0b87a559f481243ae342282607171d5ef32a
HorseMeatMan <sisonosovietgus@gmail.com>
2025-04-20 23:57:57 +0200

    deleted the 'button' function (no need to build and draw convexshapes rn), added a test code to check if cursor is over the sprite

--- main.cpp
+++ main.cpp
@@ -2,28 +2,6 @@
 #include <SFML/Window.hpp>
 #include <iostream>
 
-sf::ConvexShape* buttons(float coords[2]) {
-
-    float x = coords[0];
-    float y = coords[1];
-    int z = 1;
-
-
-    sf::ConvexShape b1;
-    b1.setPointCount(4);
-    b1.setPoint(0, sf::Vector2f((x+0) * z, (y + 0) * z));
-    b1.setPoint(1, sf::Vector2f((x + 0) * z, (y + 10) * z));
-    b1.setPoint(2, sf::Vector2f((x + 25) * z, (y + 10) * z));
-    b1.setPoint(3, sf::Vector2f((x + 25) * z, (y + 0) * z));
-    b1.setOutlineColor(sf::Color::Red);
-    b1.setOutlineThickness(5);
-    b1.setPosition({ 10, 20 });
-
-    static sf::ConvexShape arr[1] = { b1 };
-    return arr;
-
-
-}
 int main() {
     
     sf::RenderWindow window(sf::VideoMode({ 1920, 1080 }), "BresaolaMat 2.0.0");
@@ -35,12 +13,9 @@ int main() {
     }
     
     sf::Sprite sprite(texture);
-    
-    float x = 10;
-    float y = 500;
-    float coords[2] = { x, y};
 
-    sf::ConvexShape* arr = buttons(coords);
+    sf::FloatRect b1 = sprite.sf::Sprite::getGlobalBounds();
+   
     
     while (window.isOpen())
     {
@@ -49,10 +24,14 @@ int main() {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
+        
+        sf::Vector2i mousepos = sf::Mouse::getPosition(window);
+        if (b1.contains(static_cast<sf::Vector2f>(mousepos))) {
+            std::cout << "in ";
+        }
 
         window.clear();
         window.draw(sprite);
-        window.draw(arr[0]);
         window.display();
     }
     
